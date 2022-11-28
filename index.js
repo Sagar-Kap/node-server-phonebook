24867b772102bd841940748364bbb1641dee2b28
Sagar Kapoor <sagrkapr@gmail.com>
2022-11-28 14:26:18 +0200

    ✅ Restored index.js prior to push for Ex13.13-14

--- index.js
+++ index.js
@@ -18,6 +18,40 @@ app.use(
   )
 );
 
+let data = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+  {
+    id: 5,
+    name: "Chal hat",
+    number: "100178",
+  },
+];
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
